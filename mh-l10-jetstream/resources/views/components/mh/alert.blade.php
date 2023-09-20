0294e4f7cb5f68ecdb2147506e636c6a269b9167
Manuel Henriquez <manuelhm1993@gmail.com>
2023-09-20 09:41:01 -0400

    Pasar valores al componente a través de atributos y recibirlos con la directiva props para volverlos variables

--- mh-l10-jetstream/resources/views/components/mh/alert.blade.php
+++ mh-l10-jetstream/resources/views/components/mh/alert.blade.php
@@ -1,4 +1,6 @@
-<div class="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4" role="alert">
-    <p class="font-bold">Be Warned</p>
+@props(['title'])
+
+<article class="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4" role="alert">
+    <h2 class="font-bold">{{ $title }}</h2>
     {{ $slot }}
-</div>
+</article>
