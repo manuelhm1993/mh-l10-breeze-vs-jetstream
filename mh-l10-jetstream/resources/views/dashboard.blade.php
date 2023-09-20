eb6fb39d9b92e51f26d35ca408e27b1dcc9b4bbf
Manuel Henriquez <manuelhm1993@gmail.com>
2023-09-20 10:02:37 -0400

    Pasando variables php como atributos al componente

--- mh-l10-jetstream/resources/views/dashboard.blade.php
+++ mh-l10-jetstream/resources/views/dashboard.blade.php
@@ -8,7 +8,11 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
-                <x-mh.alert>
+                @php
+                    $type = 'danger';
+                @endphp
+
+                <x-mh.alert :type="$type">
                     <x-slot:title>
                         Título desde el slot con nombre
                     </x-slot:title>
