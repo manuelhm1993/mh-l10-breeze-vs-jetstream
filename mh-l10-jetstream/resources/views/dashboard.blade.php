ef8bb84f7f237979b151a03cd084693a58a3182c
Manuel Henriquez <manuelhm1993@gmail.com>
2023-09-20 09:59:45 -0400

    Valor por defecto del atributo type, props permite darle valores por defecto a sus atributos al ser definidos como un array asociativo

--- mh-l10-jetstream/resources/views/dashboard.blade.php
+++ mh-l10-jetstream/resources/views/dashboard.blade.php
@@ -8,7 +8,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
-                <x-mh.alert type="danger">
+                <x-mh.alert>
                     <x-slot:title>
                         Título desde el slot con nombre
                     </x-slot:title>
