92a69bac1cd049c8b03a25886933f90b50eb31d9
Manuel Henriquez <manuelhm1993@gmail.com>
2023-09-20 09:48:35 -0400

    Slot con nombre Sirven para pasar información adicional o contenido extra para no sobrecargarse de propiedades

--- mh-l10-jetstream/resources/views/components/mh/alert.blade.php
+++ mh-l10-jetstream/resources/views/components/mh/alert.blade.php
@@ -1,5 +1,3 @@
-@props(['title'])
-
 <article class="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4" role="alert">
     <h2 class="font-bold">{{ $title }}</h2>
     {{ $slot }}
--- mh-l10-jetstream/resources/views/dashboard.blade.php
+++ mh-l10-jetstream/resources/views/dashboard.blade.php
@@ -8,7 +8,11 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
-                <x-mh.alert title="Título de prueba">
+                <x-mh.alert>
+                    <x-slot:title>
+                        Título desde el slot con nombre
+                    </x-slot:title>
+
                     <p>Este es un texto de prueba</p>
                 </x-mh.alert>
             </div>
