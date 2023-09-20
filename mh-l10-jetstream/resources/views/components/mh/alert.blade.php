3745718e24384e1fb32201cfbcd6dfce3c436901
Manuel Henriquez <manuelhm1993@gmail.com>
2023-09-20 09:57:39 -0400

    Volver a usar la directiva props En este caso se usó el tipo de alert para cambiar su color dinámicamente

--- mh-l10-jetstream/resources/views/components/mh/alert.blade.php
+++ mh-l10-jetstream/resources/views/components/mh/alert.blade.php
@@ -1,4 +1,20 @@
-<article class="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4" role="alert">
+@props(['type'])
+
+@php
+    switch ($type) {
+        case 'info':
+            $clases = 'bg-blue-100 border-blue-500 text-blue-700';
+            break;
+        case 'danger':
+            $clases = 'bg-red-100 border-red-500 text-red-700';
+            break;
+        default:
+            $clases = 'bg-gray-100 border-gray-500 text-gray-700';
+            break;
+    }
+@endphp
+
+<article class="{{ $clases }} border-l-4 p-4" role="alert">
     <h2 class="font-bold">{{ $title }}</h2>
     {{ $slot }}
 </article>
