9570a46f9c399efd4c88c7d7a316eb824912a7cc
Manuel Henriquez <manuelhm1993@gmail.com>
2023-09-20 09:20:25 -0400

    Primer componente Se definió el componente alert con una base de la biblioteca tailwind Se definió el slot que es el contenido variable Se llamó desde el dashboard ese componente pasándole contenido por parámetro al slot

--- mh-l10-jetstream/resources/views/components/mh/alert.blade.php
+++ mh-l10-jetstream/resources/views/components/mh/alert.blade.php
@@ -1,3 +1,4 @@
-<div>
-    <!-- Well begun is half done. - Aristotle -->
+<div class="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4" role="alert">
+    <p class="font-bold">Be Warned</p>
+    {{ $slot }}
 </div>
--- mh-l10-jetstream/resources/views/dashboard.blade.php
+++ mh-l10-jetstream/resources/views/dashboard.blade.php
@@ -8,7 +8,9 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
-
+                <x-mh.alert>
+                    <p>Este es un texto de prueba</p>
+                </x-mh.alert>
             </div>
         </div>
     </div>
