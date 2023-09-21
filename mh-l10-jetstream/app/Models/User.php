9d718671077300332ddecfe010ccb9b7e5562384
Manuel Henriquez <manuelhm1993@gmail.com>
2023-09-21 05:03:54 -0400

    Activar los timestamps en la tabla pivot

--- mh-l10-jetstream/app/Models/User.php
+++ mh-l10-jetstream/app/Models/User.php
@@ -91,6 +91,6 @@ class User extends Authenticatable
      */
     public function roles(): BelongsToMany
     {
-        return $this->belongsToMany(Role::class);
+        return $this->belongsToMany(Role::class)->withTimestamps();
     }
 }
